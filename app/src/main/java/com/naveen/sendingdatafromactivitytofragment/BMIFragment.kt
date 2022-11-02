c290a5694f5ed4e637d7c6deb23c9141e1c7dc82
thephotogenicbug <navinkumarachari@gmail.com>
2022-11-02 17:37:03 +0530

    Finalize app

--- app/src/main/java/com/naveen/sendingdatafromactivitytofragment/BMIFragment.kt
+++ app/src/main/java/com/naveen/sendingdatafromactivitytofragment/BMIFragment.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class BMIFragment : Fragment() {
 
+    lateinit var result : TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val view : View = inflater.inflate(R.layout.fragment_b_m_i, container, false)
+
+        result = view.findViewById(R.id.textViewResult)
+
+       val weight = arguments?.getInt("weight")!!.toInt()
+       val height = arguments?.getInt("height")!!.toInt()
+
+       val bmi : Double = ((weight * 10000) / (height * weight)).toDouble()
+
+       result.text = " Your BMI is $bmi "
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_b_m_i, container, false)
+        return view
     }
 }
--- app/src/main/java/com/naveen/sendingdatafromactivitytofragment/MainActivity.kt
+++ app/src/main/java/com/naveen/sendingdatafromactivitytofragment/MainActivity.kt
@@ -2,10 +2,41 @@ package com.naveen.sendingdatafromactivitytofragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etWeight : EditText
+    lateinit var etHeight : EditText
+    lateinit var calculate : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etWeight = findViewById(R.id.editTextWeight)
+        etHeight = findViewById(R.id.editTextHeight)
+        calculate = findViewById(R.id.buttonCalculate)
+
+        val fm : FragmentManager = supportFragmentManager
+        val ft : FragmentTransaction = fm.beginTransaction()
+        val bmiFragment = BMIFragment()
+
+        calculate.setOnClickListener {
+
+            val weight = etWeight.text.toString().toInt()
+            val height = etHeight.text.toString().toInt()
+
+            val bundle = Bundle()
+            bundle.putInt("weight", weight)
+            bundle.putInt("height", height)
+
+            bmiFragment.arguments = bundle
+
+            ft.add(R.id.frame, bmiFragment)
+
+            ft.commit()
+        }
     }
 }
